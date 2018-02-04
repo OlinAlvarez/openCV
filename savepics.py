42f798bdfe405ec46924a477df96acd239dd61a9
OlinAlvarez <olinalvarez@gmail.com>
2018-02-04 12:06:17 -0800

    added savepics so we can run it on cameras to save pictures'

--- savepics.py
+++ savepics.py
@@ -7,6 +7,8 @@
      python savepics.py dirname filename
  videocapture 1 sets it to work with usb camera
  if it's set to 0 it will capture from webcam
+
+ change secs to change the image capture rate
 '''
 import sys
 import time
@@ -15,13 +17,14 @@ import cv2
 cap = cv2.VideoCapture(1)
 directory =  sys.argv[1]
 name =  sys.argv[2]
+secs = 5
 
 while True:
     _,frame = cap.read()
 
     curr_time = int(round(time.time() * 1000))
     cv2.imshow('frame',frame)
-    if curr_time % 5 == 0:
+    if curr_time % secs == 0:
         cv2.imwrite('yellow_buoy_images/yellow_buoy_' + str(curr_time) + '.jpg',frame)
         cv2.imwrite(directory +'/' + name + str(curr_time) + '.jpg',frame)
 
