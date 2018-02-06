92c4b94597bf26ce8cc182274e39ea2e765f1065
OlinAlvarez <olinalvarez@gmail.com>
2018-02-05 19:03:15 -0800

    fixed the taskManager to be an actual class

--- taskManager.py
+++ taskManager.py
@@ -6,23 +6,24 @@ import detect_cashIn
 This is the class which will handle all the tasks.
 Gate,Dice,Roulette,CashIn
 '''
-def __init__(self,task):
-    task = task.lower()
-    if(task == 'gate'):
-        gateDetect()
-    elif(task == 'dice'):
-        diceDetect()
-    elif(task == 'roulette'):
-        rouletteDetect()
-    elif(task == 'cashin'):
-        cashInDetect()
-    else:
-        print 'Error task not found'
-def gateDetect():
-    return
-def diceDetect():
-    return
-def rouletteDetect():
-    return
-def cashInDetect():
-    return
+class TaskManager():
+    def __init__(self,task):
+        task = task.lower()
+        if(task == 'gate'):
+            self.gateDetect()
+        elif(task == 'dice'):
+            self.diceDetect()
+        elif(task == 'roulette'):
+            self.rouletteDetect()
+        elif(task == 'cashin'):
+            self.cashInDetect()
+        else:
+            print 'Error task not found'
+    def gateDetect(self):
+        return
+    def diceDetect(self):
+        return
+    def rouletteDetect(self):
+        return
+    def cashInDetect(self):
+        return
